e3f43dd476280b649bd1da8856b61083d7276206
John Zanchetta <johnzan@gmail.com>
2011-04-29 13:19:44 +0200

    Nested Model Form updated for Rails 3.0.5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surveysays::Application.config.secret_token = '7d996971640b6cef7ecd73dcebee3b0844fcaee2f1afc02d57fafddde7c63a9245baf2a7b161a990f12de6f4ed31f23378928fbc0d4a0fe3ea32976dcbb9952a'
